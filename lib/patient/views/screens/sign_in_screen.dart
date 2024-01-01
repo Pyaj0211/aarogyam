bf09349527546cb824a302230736f272380cdeff
pavanlashkari <pavanlashkari27@gmail.com>
2024-01-01 20:03:14 +0530

    just for testing

---
+++ lib/patient/views/screens/sign_in_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+class SignInPatient extends StatelessWidget {
+  const SignInPatient({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //just for testing
+    );
+  }
+}
