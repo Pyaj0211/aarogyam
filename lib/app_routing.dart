d444a235612a06df4e4e809fe4346af3ed7e9e5a
yashdip <jethavayashdip5@gmail.com>
2024-01-03 12:29:26 +0530

    firebase connected

--- lib/app_routing.dart
+++ lib/app_routing.dart
@@ -1,11 +1,11 @@
-import 'package:aarogyam/main.dart';
+import 'package:aarogyam/doctor/views/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 class AppRouting{
   
   Route? onGenerateRoute(RouteSettings routeSettings){
     switch (routeSettings.name){
       case '/':
-        return MaterialPageRoute(builder: (_)=> const MyApp());
+        return MaterialPageRoute(builder: (_)=> const LoginScreen());
       default:
         return null;
     }
